14bb325350e3819745f0ddc02c64681ccfab01a2
apostol-roman <57871278+apostol-roman@users.noreply.github.com>
2019-11-25 20:11:57 +0500

    Add files via upload

---
+++ settings_migrator.js
@@ -0,0 +1,484 @@
+﻿const DefaultSettings = {
+    "enabled":  true,
+    "alerted":  true, // Pop-up notification
+    "notice":   true, // Pop-up notification
+    "messager": true, // Message
+    "marker":   true, // Lootbeam
+    "itemId":  98260, // Vergo's Head
+    "bosses": [
+            {
+                "huntingZoneId": 1023,
+                "templateId": 3000,
+                "name": "(Ивент) Тень Дуриона"
+            },
+            {
+                "huntingZoneId": 1023,
+                "templateId": 20150805,
+                "name": "(Ивент) Тень Дуриона"
+            },
+            {
+                "huntingZoneId": 1023,
+                "templateId": 88888888,
+                "name": "(Ивент) Сундук с сокровищами"
+            },
+            {
+                "huntingZoneId": 1023,
+                "templateId": 88888889,
+                "name": "(Ивент) Сундук с сокровищами"
+            },
+            {
+                "huntingZoneId": 1023,
+                "templateId": 160341,
+                "name": "(Ивент) Дед Мороз"
+            },
+            {
+                "huntingZoneId": 1023,
+                "templateId": 99999997,
+                "name": "(Ивент) Жирный Санта"
+            },
+            {
+                "huntingZoneId": 1023,
+                "templateId": 99999998,
+                "name": "(Ивент) Крошечный Санта"
+            },
+            {
+                "huntingZoneId": 1023,
+                "templateId": 99999991,
+                "name": "(Ивент) Кайман похититель яиц"
+            },
+            {
+                "huntingZoneId": 1023,
+                "templateId": 99999992,
+                "name": "(Ивент) Кайман похититель яиц"
+            },
+            {
+                "huntingZoneId": 1023,
+                "templateId": 99999999,
+                "name": "(Ивент) Кайман похититель яиц"
+            },
+            {
+                "huntingZoneId": 183,
+                "templateId": 6002,
+                "name": "(Ивент) AirDrop"
+            },
+            {
+                "huntingZoneId": 2,
+                "templateId": 1271,
+                "name": "Аркун (Аркадия - Дивный лес)"
+            },
+            {
+                "huntingZoneId": 3,
+                "templateId": 1271,
+                "name": "Аркун (Аркадия - Леса Забвения)"
+            },
+            {
+                "huntingZoneId": 5,
+                "templateId": 1271,
+                "name": "Аркун (Аркадия - Трясина Туванги)"
+            },
+            {
+                "huntingZoneId": 6,
+                "templateId": 1271,
+                "name": "Аркун (Аркадия - Долина Титанов)"
+            },
+            {
+                "huntingZoneId": 7,
+                "templateId": 1271,
+                "name": "Аркун (Аркадия - Небесные холмы)"
+            },
+            {
+                "huntingZoneId": 4,
+                "templateId": 1271,
+                "name": "Иторо (Остгарат - Фирмаунт)"
+            },
+            {
+                "huntingZoneId": 9,
+                "templateId": 1271,
+                "name": "Иторо (Остгарат - Долина Вознесения)"
+            },
+            {
+                "huntingZoneId": 10,
+                "templateId": 1271,
+                "name": "Иторо (Остгарат - Остров Серпентис)"
+            },
+            {
+                "huntingZoneId": 11,
+                "templateId": 1271,
+                "name": "Иторо (Остгарат - Изрезанный берег)"
+            },
+            {
+                "huntingZoneId": 12,
+                "templateId": 1271,
+                "name": "Иторо (Остгарат - Остров Мистмур)"
+            },
+            {
+                "huntingZoneId": 15,
+                "templateId": 1271,
+                "name": "Форета (Попория - Утесы Безумия)"
+            },
+            {
+                "huntingZoneId": 16,
+                "templateId": 1271,
+                "name": "Форета (Попория - Долина Клыка)"
+            },
+            {
+                "huntingZoneId": 17,
+                "templateId": 1271,
+                "name": "Форета (Попория - Ущелье Параанон)"
+            },
+            {
+                "huntingZoneId": 23,
+                "templateId": 1271,
+                "name": "Форета (Попория - Озеро Слез)"
+            },
+            {
+                "huntingZoneId": 18,
+                "templateId": 1271,
+                "name": "Виадун (Вал-Ауреум - Исполинские Развалины)"
+            },
+            {
+                "huntingZoneId": 19,
+                "templateId": 1271,
+                "name": "Виадун (Вал-Ауреум - Вольноземье)"
+            },
+            {
+                "huntingZoneId": 21,
+                "templateId": 1271,
+                "name": "Виадун (Вал-Ауреум - Утес Василисков)"
+            },
+            {
+                "huntingZoneId": 24,
+                "templateId": 1271,
+                "name": "Виадун (Вал-Ауреум - Аурумская Дорога)"
+            },
+            {
+                "huntingZoneId": 26,
+                "templateId": 1271,
+                "name": "Эсат (Эссения - Блаженное Озеро)"
+            },
+            {
+                "huntingZoneId": 27,
+                "templateId": 1271,
+                "name": "Эсат (Эссения - Эссенийский Хребет)"
+            },
+            {
+                "huntingZoneId": 28,
+                "templateId": 1271,
+                "name": "Эсат (Эссения - Гибельный Лес)"
+            },
+            {
+                "huntingZoneId": 29,
+                "templateId": 1271,
+                "name": "Эсат (Эссения - Извечный Лес)"
+            },
+            {
+                "huntingZoneId": 30,
+                "templateId": 1271,
+                "name": "Вэлса (Район Веритас - Убежище Балдера)"
+            },
+            {
+                "huntingZoneId": 31,
+                "templateId": 1271,
+                "name": "Сторан (Вестония - Предел Бурь)"
+            },
+            {
+                "huntingZoneId": 32,
+                "templateId": 1271,
+                "name": "Сторан (Вестония - Гора Тираннас)"
+            },
+            {
+                "huntingZoneId": 34,
+                "templateId": 1271,
+                "name": "Сторан (Вестония - Морозный Предел)"
+            },
+            {
+                "huntingZoneId": 35,
+                "templateId": 1271,
+                "name": "Виас (Вал-Элениум - Вирмово Ущелье)"
+            },
+            {
+                "huntingZoneId": 36,
+                "templateId": 1271,
+                "name": "Виас (Вал-Элениум - Тор-Эксул)"
+            },
+            {
+                "huntingZoneId": 38,
+                "templateId": 1271,
+                "name": "Виас (Вал-Элениум - Каньон Сиенна)"
+            },
+            {
+                "huntingZoneId": 40,
+                "templateId": 1271,
+                "name": "Ваннева (Вал-Палрада - Зона Карантина)"
+            },
+            {
+                "huntingZoneId": 41,
+                "templateId": 1271,
+                "name": "Ваннева (Вал-Палрада - Свирепая Долина)"
+            },
+            {
+                "huntingZoneId": 45,
+                "templateId": 1271,
+                "name": "Лотика (Вал-Тиркай - Питомник Аргонов)"
+            },
+            {
+                "huntingZoneId": 47,
+                "templateId": 1271,
+                "name": "Лотика (Вал-Тиркай - Лес Тиркай)"
+            },
+            {
+                "huntingZoneId": 48,
+                "templateId": 1271,
+                "name": "Хелкун (Район Хелкан - Хановарские предместья)"
+            },
+            {
+                "huntingZoneId": 49,
+                "templateId": 1271,
+                "name": "Тезлуар (Вал-Кэли - Аргония)"
+            },
+            {
+                "huntingZoneId": 50,
+                "templateId": 1271,
+                "name": "Тезлуар (Вал-Кэли - Гранаркус)"
+            },
+            {
+                "huntingZoneId": 51,
+                "templateId": 1271,
+                "name": "Лоакун (Лоркада - Долина Пиков)"
+            },
+            {
+                "huntingZoneId": 52,
+                "templateId": 1271,
+                "name": "Лоакун (Лоркада - Долина Проклятых)"
+            },
+            {
+                "huntingZoneId": 54,
+                "templateId": 1271,
+                "name": "Силвета (Силванот - Силивуд)"
+            },
+            {
+                "huntingZoneId": 55,
+                "templateId": 1271,
+                "name": "Силвета (Силванот - Дрожащий Лес)"
+            },
+            {
+                "huntingZoneId": 56,
+                "templateId": 1271,
+                "name": "Силвета (Силванот - Шепчущие Леса)"
+            },
+            {
+                "huntingZoneId": 57,
+                "templateId": 1271,
+                "name": "Силвета (Силванот - Амена-Кватла)"
+            },
+            {
+                "huntingZoneId": 172,
+                "templateId": 1271,
+                "name": "Варэку (Вал-Орин - Дикарский Предел)"
+            },
+            {
+                "huntingZoneId": 181,
+                "templateId": 1271,
+                "name": "Варэку (Вал-Орин - Экс-Прима)"
+            },
+            {
+                "huntingZoneId": 182,
+                "templateId": 1271,
+                "name": "Варэку (Вал-Орин - Долина Источников)"
+            },
+            {
+                "huntingZoneId": 183,
+                "templateId": 1278,
+                "name": "Варэку (Вал-Орин - Окрестности Верхнего Дозора)"
+            },
+            {
+                "huntingZoneId": 191,
+                "templateId": 1271,
+                "name": "Варэку (Вал-Орин - Аркс-Умбра)"
+            },
+            {
+                "huntingZoneId": 13,
+                "templateId": 1271,
+                "name": "Бардун (Остров Зари)"
+            },
+            {
+                "huntingZoneId": 63,
+                "templateId": 1278,
+                "name": "Веракун (Велика)"
+            },
+            {
+                "huntingZoneId": 72,
+                "templateId": 1278,
+                "name": "Аллума (Аллемантея)"
+            },
+            {
+                "huntingZoneId": 84,
+                "templateId": 1278,
+                "name": "Кай Тера (Кайатор)"
+            },
+            {
+                "huntingZoneId": 63,
+                "templateId": 1271,
+                "name": "Питер Бэйл (Велика)"
+            },
+            {
+                "huntingZoneId": 63,
+                "templateId": 1279,
+                "name": "Питер Бэйл (Велика)"
+            },
+            {
+                "huntingZoneId": 72,
+                "templateId": 1271,
+                "name": "Хью Моск (Аллемантея)"
+            },
+            {
+                "huntingZoneId": 84,
+                "templateId": 1271,
+                "name": "Кейл Лунный свет (Кайатор)"
+            },
+            {
+                "huntingZoneId": 183,
+                "templateId": 1271,
+                "name": "Джон Доу (Верхний Дозор)"
+            },
+            {
+                "huntingZoneId": 63,
+                "templateId": 1276,
+                "logTime": 0,
+                "name": "Гоблин, ответственный за доставку (Велика)"
+            },
+            {
+                "huntingZoneId": 63,
+                "templateId": 1284,
+                "name": "Гоблин, ответственный за доставку (Велика)"
+            },
+            {
+                "huntingZoneId": 84,
+                "templateId": 1276,
+                "logTime": 0,
+                "name": "Гоблин, ответственный за доставку (Кайатор)"
+            },
+            {
+                "huntingZoneId": 72,
+                "templateId": 1276,
+                "logTime": 0,
+                "name": "Гоблин, ответственный за доставку (Аллемантея)"
+            },
+            {
+                "huntingZoneId": 183,
+                "templateId": 1276,
+                "logTime": 0,
+                "name": "Гоблин, ответственный за доставку (Верхний Дозор)"
+            },
+            {
+                "huntingZoneId": 26,
+                "templateId": 5001,
+                "logTime": 0,
+                "name": "Ортан (Эссения - Блаженное Озеро)"
+            },
+            {
+                "huntingZoneId": 39,
+                "templateId": 501,
+                "logTime": 0,
+                "name": "Хазар (Вал-Палрада - Зона Карантина)"
+            },
+            {
+                "huntingZoneId": 51,
+                "templateId": 4001,
+                "logTime": 0,
+                "name": "Кэлос (Лоркада - Долина Пиков)"
+            },
+            {
+                "huntingZoneId": 2020,
+                "templateId": 1100,
+                "name": "Supreme Draakon Pursuer"
+            },
+            {
+                "huntingZoneId": 2020,
+                "templateId": 1101,
+                "name": "Draakon Guard-Captain"
+            },
+            {
+                "huntingZoneId": 2020,
+                "templateId": 1102,
+                "name": "Supreme Draakon Dominator"
+            },
+            {
+                "huntingZoneId": 2020,
+                "templateId": 1200,
+                "name": "Elite Naga Priest"
+            },
+            {
+                "huntingZoneId": 2020,
+                "templateId": 1201,
+                "name": "Elite Naga Knight"
+            },
+            {
+                "huntingZoneId": 2020,
+                "templateId": 1202,
+                "name": "Elite Naga Executioner"
+            },
+            {
+                "huntingZoneId": 2020,
+                "templateId": 1300,
+                "name": "Manuk"
+            },
+            {
+                "huntingZoneId": 2020,
+                "templateId": 1400,
+                "name": "Axcellob"
+            },
+            {
+                "huntingZoneId": 2020,
+                "templateId": 1500,
+                "name": "Zemces"
+            },
+            {
+                "huntingZoneId": 2020,
+                "templateId": 1600,
+                "name": "Red-Flecked Crab"
+            },
+            {
+                "huntingZoneId": 2020,
+                "templateId": 1601,
+                "name": "Thorny Eyestalker"
+            },
+            {
+                "huntingZoneId": 2020,
+                "templateId": 1700,
+                "name": "Vekes"
+            }
+        ]
+};
+
+module.exports = function MigrateSettings(from_ver, to_ver, settings) {
+    if (from_ver === undefined) {
+        // Migrate legacy config file
+        return Object.assign(Object.assign({}, DefaultSettings), settings);
+    } else if (from_ver === null) {
+        // No config file exists, use default settings
+        return DefaultSettings;
+    } else {
+        // Migrate from older version (using the new system) to latest one
+        if (from_ver + 1 < to_ver) { // Recursively upgrade in one-version steps
+            settings = MigrateSettings(from_ver, from_ver + 1, settings);
+            return MigrateSettings(from_ver + 1, to_ver, settings);
+        }
+        // If we reach this point it's guaranteed that from_ver === to_ver - 1, so we can implement
+        // a switch for each version step that upgrades to the next version. This enables us to
+        // upgrade from any version to the latest version without additional effort!
+        switch (to_ver) {
+            default:
+                let oldsettings = settings
+                settings = Object.assign(DefaultSettings, {});
+                for (let option in oldsettings) {
+                    if (option == "bosses") continue
+                    if (settings[option]) {
+                        settings[option] = oldsettings[option]
+                    }
+                }
+                break;
+        }
+        return settings;
+    }
+}
